8d69db785918194e4e2474ddaba4b2fb1def1c0f
Кирилл <kirillmorgunov@mail.ru>
2020-05-05 20:29:19 +0300

    с помощью пакета imagemin сконвертированы картинки в формат webP. добавлены source webp для каждой контентной картинки

--- webp.js
+++ webp.js
@@ -1,17 +1,28 @@
-var imagemin = require("imagemin"),    // The imagemin module.
-  webp = require("imagemin-webp"),   // imagemin's WebP plugin.
-  outputFolder = "./img",            // Output folder
-  PNGImages = "./img/*.png",         // PNG images
-  JPEGImages = "./img/*.jpg";        // JPEG images
+// var imagemin = require("imagemin"),    // The imagemin module.
+//   webp = require("imagemin-webp"),   // imagemin's WebP plugin.
+//   outputFolder = "./img",            // Output folder
+//   PNGImages = "./img/*.png",         // PNG images
+//   JPEGImages = "./img/*.jpg";        // JPEG images
 
-imagemin([PNGImages], outputFolder, {
-  plugins: [webp({
-      lossless: true // Losslessly encode images
-  })]
-});
+// imagemin([PNGImages], outputFolder, {
+//   plugins: [webp({
+//       lossless: true // Losslessly encode images
+//   })]
+// });
 
-imagemin([JPEGImages], outputFolder, {
-  plugins: [webp({
-    quality: 65 // Quality setting from 0 to 100
-  })]
+// imagemin([JPEGImages], outputFolder, {
+//   plugins: [webp({
+//     quality: 65 // Quality setting from 0 to 100
+//   })]
+// });
+
+
+
+const path = require('path');
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+
+imagemin(['source/img/*.{jpg,png}'], {
+  destination: path.resolve(__dirname, 'source/webp'),
+  plugins: [imageminWebp({ quality: 50 })],
 });
